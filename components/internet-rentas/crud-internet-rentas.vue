b76eb955592684afc6a4517ea560b7ebf00a62b3
manuel lopez <manuellh2008@gmail.com>
2020-09-09 02:26:31 -0500

    agregada funcionalidad de agregar y eliminar dispostivos y rentas, y corregido formato de fechas

--- components/internet-rentas/crud-internet-rentas.vue
+++ components/internet-rentas/crud-internet-rentas.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    v-dialog( v-model="modalNuevaRenta" )
+    v-dialog( v-model="modalNuevaRenta" @input="cambioModalNuevaRenta")
       template(v-slot:activator = "{on, attrs}")
         v-btn(
           v-on="on"
@@ -11,38 +11,116 @@
         v-card-text
           v-container
             v-row
-              v-col
-                v-text-field(label = "Consola")
+              v-col(cols="12" sm="12")
+                v-select(
+                  label="Dispositivo"
+                  :items="dispositivos"
+                  item-text="nombre"
+                  item-value="id"
+                  v-model="nuevaRenta.movilDispositivoId"
+                )
+              v-col(cols="12" sm="6")
+                v-text-field(
+                  label="Fecha fin"
+                  type="date"
+                  @change="cambioFechaFin()"
+                  v-model="fechaFin"
+                )
+              v-col(cols="12" sm="6")
+                v-text-field(
+                  label="Dinero"
+                  type="number"
+                  v-model.number="nuevaRenta.dinero"
+                )
         v-card-actions 
           v-spacer
           v-btn( @click="modalNuevaRenta = false" ) Cerrar
-          v-btn( @click="" ) Agregar
+          v-btn( @click="agregarRenta()" ) Agregar
     v-data-table(
       :headers = "headers",
       :items = "rentas"
     )
       template(v-slot:item.fechaFin="{item}")
         span {{$moment.utc(item.fechaFin).local().format('YYYY MMMM DD dddd')}}
-      
+      //- template( v-slot:item.acciones = "{ item }" )
+      //-   v-btn( @click="mostrarModalNuevaRenta(item.id)" ) Editar
+      template( v-slot:item.acciones = "{ item }" )
+        v-dialog(
+          v-model="modalEliminarRenta"
+          :retain-focus="false"
+        )
+          template( v-slot:activator = "{ on, attrs }" )
+            v-btn(
+              v-on = "on"
+              v-bind = "attrs"
+            ) Eliminar
+          v-card
+            v-card-title Eliminar
+            v-card-actions
+              v-spacer
+              v-btn( @click="modalEliminarRenta = false" ) Cerrar
+              v-btn( @click="eliminarRenta(item.id)" ) Eliminar
 </template>
 
 <script>
 export default {
   data: () => ({
     modalNuevaRenta: false,
+    modalEliminarRenta: false,
     headers: [
       {text: 'Dispositivo', value: 'movilDispositivo.nombre'},
       {text: 'Fecha Fin', value: 'fechaFin'},
-      {text: 'Dinero', value: 'dinero'}
+      {text: 'Dinero', value: 'dinero'},
+      {text: 'Acciones', value: 'acciones'}
     ],
-    rentas: []
+    rentas: [],
+    dispositivos: [],
+    nuevaRenta: {
+      movilDispositivoId: "",
+      fechaFin: "",
+      dinero: ""
+    },
+    fechaFin: ""
   }),
   created() {
     this.llenarRentas()
   },
   methods: {
+    async eliminarRenta(id) {
+      this.modalEliminarRenta = false
+      this.$axios.delete(`/internetRentas/${id}`).then(() => {
+        this.llenarRentas()
+      })
+    },
+    cambioFechaFin() {
+      this.nuevaRenta.fechaFin = this.$moment(this.fechaFin)
+    },
+    cambioModalNuevaRenta(visible) {
+      if (visible) {
+        this.llenarDispositivos()
+      }
+    },
+    limpiarNuevaRenta() {
+      this.nuevaRenta = {
+        movilDispositivoId: "",
+        fechaFin: "",
+        dinero: ""
+      }
+      this.fechaFin = ""
+    },
+    async llenarDispositivos() {
+      this.dispositivos = await this.$axios.$get('/movilDispositivos')
+    },
     async llenarRentas() {
-      this.rentas = await this.$axios.$get('/internetrentas')
+      this.rentas = await this.$axios.$get('/internetRentas')
+    },
+    async agregarRenta() {
+      console.log(this.nuevaRenta)
+      this.modalNuevaRenta = false
+      this.$axios.post('/internetrentas', this.nuevaRenta).then(() => {
+        this.llenarRentas()
+        this.limpiarNuevaRenta()
+      })
     }
   }
 }
--- components/movil-dispositivos/crud-movil-dispositivos.vue
+++ components/movil-dispositivos/crud-movil-dispositivos.vue
@@ -1,17 +1,58 @@
 <template lang="pug">
   div
+    v-dialog( v-model="modalDispositivo" )
+      template( v-slot:activator = "{ on, attrs }" )
+        v-btn(
+          v-on = "on",
+          v-bind = "attrs"
+        ) Agregar
+      v-card(  )
+        v-card-title Agregar
+        v-card-text
+          v-container
+            v-row
+              v-col
+                v-text-field( v-model="nuevoDispositivo.nombre"  label = "Nombre" )
+              v-col
+                v-text-field( v-model="nuevoDispositivo.mac"  label = "MAC" )
+        v-card-actions
+          v-btn( @click="modalDispositivo = false" ) Cerrar
+          v-btn( @click="agregarDispositivo()" ) Agregar
     v-data-table(
       :headers = "headers",
       :items = "dispositivos"
     )
+      template(v-slot:item.acciones="{ item }")
+        v-dialog(
+          v-model="modalEliminarDispositivo"
+          :retain-focus="false"
+        )
+          template( v-slot:activator = "{ on, attrs }" )
+            v-btn(
+              v-on = "on"
+              v-bind = "attrs"
+            ) Eliminar
+          v-card
+            v-card-title Eliminar
+            v-card-actions
+              v-spacer
+              v-btn( @click="modalEliminarDispositivo = false" ) Cerrar
+              v-btn( @click="eliminarDispositivo(item.id)" ) Eliminar
 </template>
 
 <script>
 export default {
   data: () => ({
+    modalEliminarDispositivo: false,
+    modalDispositivo: false,
+    nuevoDispositivo: {
+      nombre: "",
+      mac: ""
+    },
     headers: [
       {text: 'Nombre', value: 'nombre'},
-      {text: 'MAC', value: 'mac'}
+      {text: 'MAC', value: 'mac'},
+      {text: 'Acciones', value: 'acciones'}
     ],
     dispositivos: []
   }),
@@ -19,8 +60,27 @@ export default {
     this.llenarDispositivos()
   },
   methods: {
+    limpiarNuevoDispositivo() {
+      this.nuevoDispositivo = {
+        nombre: "",
+        mac: ""
+      }
+    },
+    async agregarDispositivo() {
+      this.modalDispositivo = false
+      this.$axios.post('/movildispositivos', this.nuevoDispositivo).then(() => {
+        this.llenarDispositivos()
+      })
+    },
     async llenarDispositivos() {
       this.dispositivos = await this.$axios.$get('/movildispositivos')
+    },
+    async eliminarDispositivo(id) {
+      this.modalEliminarDispositivo = false
+      this.$axios.delete(`/movildispositivos/${id}`).then(() => {
+        this.llenarDispositivos()
+        this.limpiarNuevoDispositivo()
+      })
     }
   }
 }
